26457e6d31d66a76d0ac650d2f40357729e8974c
drew <drew@e872ca34-ba15-0410-9420-be5960fa97e5>
2011-09-27 19:19:21 +0000

    Basic tests for helper classes
    git-svn-id: http://svn/thetan/metricCatcher/trunk@105090 e872ca34-ba15-0410-9420-be5960fa97e5

---
+++ src/test/java/com/clearspring/thetan/metricCatcher/JSONMetricTest.java
@@ -0,0 +1,40 @@
+package com.clearspring.thetan.metricCatcher;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.yammer.metrics.core.GaugeMetric;
+
+public class JSONMetricTest {
+    JSONMetric  jsonMetric;
+    
+    @Before
+    public void setUp() throws Exception {
+        jsonMetric = new JSONMetric();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testSetType_MetricType() {
+        jsonMetric.setType(MetricType.GAUGE);
+        assertEquals(MetricType.GAUGE, jsonMetric.getType());
+    }
+    
+    @Test
+    public void testSetType_TypeName() {
+        jsonMetric.setType("gauge");
+        assertEquals(MetricType.GAUGE, jsonMetric.getType());
+    }
+    
+    @Test
+    public void testGetKlass_TypeName() {
+        jsonMetric.setType("gauge");
+        assertEquals(GaugeMetric.class, jsonMetric.getMetricClass());
+    }
+}
---
+++ src/test/java/com/clearspring/thetan/metricCatcher/MetricTypeTest.java
@@ -0,0 +1,36 @@
+package com.clearspring.thetan.metricCatcher;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.yammer.metrics.core.HistogramMetric;
+
+public class MetricTypeTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testFromName() {
+        MetricType res = MetricType.fromName("meter");
+        assertEquals(MetricType.METER, res);
+    }
+
+    @Test
+    public void testFromName_Invalid() {
+        MetricType res = MetricType.fromName("foo");
+        assertEquals(null, res);
+    }
+    
+    @Test
+    public void testKlass() {
+        assertEquals(HistogramMetric.class, MetricType.HISTOGRAM.getKlass());
+    }
+}
