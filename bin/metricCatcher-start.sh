6055d1046a58283eb9633555a9660e4db51008dd
drew <drew@e872ca34-ba15-0410-9420-be5960fa97e5>
2011-10-17 21:55:36 +0000

    Define java location & use it
    git-svn-id: http://svn/thetan/metricCatcher/trunk@105762 e872ca34-ba15-0410-9420-be5960fa97e5

--- bin/metricCatcher-start.sh
+++ bin/metricCatcher-start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 JAVA=/usr/java/latest/bin/java
-nohup java -jar lib/metricCatcher-jar-with-dependencies.jar -Dlog4j.configuration=conf/log4j.properties $@ &> $(dirname $0)/../logs/metricCatcher.log &
+nohup $JAVA -jar lib/metricCatcher-jar-with-dependencies.jar -Dlog4j.configuration=conf/log4j.properties $@ &> $(dirname $0)/../logs/metricCatcher.log &
