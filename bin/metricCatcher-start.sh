d434d6c65a4e5d0a6fef160e1d845ace9bb7f77e
drew <drew@e872ca34-ba15-0410-9420-be5960fa97e5>
2011-11-08 20:49:04 +0000

    Full path for jar
    git-svn-id: http://svn/thetan/metricCatcher/trunk@106352 e872ca34-ba15-0410-9420-be5960fa97e5

--- bin/metricCatcher-start.sh
+++ bin/metricCatcher-start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 METRICCATCHER_HOME=`dirname $0`/../
 JAVA=/usr/java/latest/bin/java
-nohup $JAVA -Dlog4j.configuration=$METRICCATCHER_HOME/conf/log4j.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8089 -Dcom.sun.management.jmxremote.authenticate=false -jar lib/metricCatcher-jar-with-dependencies.jar -c $METRICCATCHER_HOME/conf/config.properties $@ &> $METRICCATCHER_HOME/logs/metricCatcher.log &
+nohup $JAVA -Dlog4j.configuration=$METRICCATCHER_HOME/conf/log4j.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8089 -Dcom.sun.management.jmxremote.authenticate=false -jar $METRICCATCHER_HOME/lib/metricCatcher-jar-with-dependencies.jar -c $METRICCATCHER_HOME/conf/config.properties $@ &> $METRICCATCHER_HOME/logs/metricCatcher.log &
