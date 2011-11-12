a18ac7a240793570c2010533532631a5aa27ec84
drew <drew@e872ca34-ba15-0410-9420-be5960fa97e5>
2011-11-12 23:00:13 +0000

    Check for Java's existence
    git-svn-id: http://svn/thetan/metricCatcher/trunk@106467 e872ca34-ba15-0410-9420-be5960fa97e5

--- bin/start-metricCatcher.sh
+++ bin/start-metricCatcher.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 METRICCATCHER_HOME=`dirname $0`/../
 JAVA=/usr/java/latest/bin/java
+if [ ! -f $JAVA ]; then
+    echo "java not found at $JAVA (set it within $0)"
+    exit 1
+fi
 nohup $JAVA -Dlog4j.configuration=$METRICCATCHER_HOME/conf/log4j.properties -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8089 -Dcom.sun.management.jmxremote.authenticate=false -jar $METRICCATCHER_HOME/lib/metricCatcher-jar-with-dependencies.jar -c $METRICCATCHER_HOME/conf/config.properties $@ &> $METRICCATCHER_HOME/logs/metricCatcher.log &
