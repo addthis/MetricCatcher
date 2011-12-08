71487e21770f43752abd4bb987df195c30447a0d
drew <drew@e872ca34-ba15-0410-9420-be5960fa97e5>
2011-12-08 18:26:45 +0000

    Extend instea of implementing
    git-svn-id: http://svn/thetan/metricCatcher/trunk@107297 e872ca34-ba15-0410-9420-be5960fa97e5

--- src/main/java/com/clearspring/thetan/metricCatcher/GaugeMetricImpl.java
+++ src/main/java/com/clearspring/thetan/metricCatcher/GaugeMetricImpl.java
@@ -2,18 +2,18 @@ package com.clearspring.thetan.metricCatcher;
 
 import com.yammer.metrics.core.GaugeMetric;
 
-public class GaugeMetricImpl implements GaugeMetric<Long> {
+public class GaugeMetricImpl extends GaugeMetric<Long> {
     long value;
-    
+
     public GaugeMetricImpl() {
         value = 0;
     }
-    
+
     @Override
     public Long value() {
         return value;
     }
-    
+
     public void setValue(long value) {
         this.value = value;
     }
